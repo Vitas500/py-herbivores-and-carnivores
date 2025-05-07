d98b6e596f3ab36fb6ec8a0c705ac26480d20151
Vitalii <vitaliy.honchar@gmail.com>
2025-05-07 17:56:46 +0300

    Solution-2

--- app/main.py
+++ app/main.py
@@ -24,5 +24,5 @@ class Carnivore(Animal):
     def bite(self, herbivore: Herbivore) -> None:
         if isinstance(herbivore, Herbivore) and not herbivore.hidden:
             herbivore.health -= 50
-            if herbivore.health <= 0:
+            if herbivore.health <= 0 and herbivore in self.alive:
                 Animal.alive.remove(herbivore)
